c6af35f580468b94856fe0f59ab70f6c64298d40
taschnue <stefan.schnuerle@hslu.ch>
2015-03-13 23:52:52 +0100

    Introduced test cases with syntax tests for SAMPLE BY - two cases failing incorrectly

--- SQLParserTest/SQLParserSkylineSamplingTests.cs
+++ SQLParserTest/SQLParserSkylineSamplingTests.cs
@@ -2,6 +2,7 @@
 using prefSQL.SQLParser;
 using prefSQL.SQLSkyline;
 using System;
+using System.Diagnostics;
 using System.Data;
 
 namespace prefSQL.SQLParserTest
@@ -9,21 +10,46 @@ namespace prefSQL.SQLParserTest
     [TestClass]
     public class SQLParserSkylineSamplingTests
     {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
 
         [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "SQLParserSkylineSamplingTests_TestSyntaxValidity.xml", "TestDataRow", DataAccessMethod.Sequential), DeploymentItem("SQLParserSkylineSamplingTests_TestSyntaxValidity.xml")]
         public void TestSyntaxValidity()
         {
-            var skylineSampleSQL = "SELECT * FROM cars_small cs SKYLINE OF cs.price LOW, cs.mileage LOW SAMPLE BY (cs.price), (cs.mileage)";
+            var skylineSampleSQL = testContextInstance.DataRow["skylineSampleSQL"].ToString();
+            Console.WriteLine(skylineSampleSQL);
+            var isExpectingException = testContextInstance.DataRow["result"].ToString() == "Fail" ? true : false;
+            Console.WriteLine(isExpectingException);
             var common = new SQLCommon();
             common.SkylineType = new SkylineSQL();
             try
             {
                 common.parsePreferenceSQL(skylineSampleSQL);
+                if (isExpectingException)
+                {
+                    Assert.Fail("Syntactically incorrect SQL Query should have thrown an Exception.");
+                }
             }
+            catch (AssertFailedException) { throw; }
             catch (Exception exception)
             {
-                Assert.Fail(String.Format("{0} - {1}"), "Syntactically correct SQL Query should not throw an Exception.", exception.Message);
+                if (!isExpectingException)
+                {
+                    Assert.Fail(String.Format("{0} - {1}", "Syntactically correct SQL Query should not have thrown an Exception.", exception.Message));
+                }
             }
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "files.csv", "files#csv", DataAccessMethod.Sequential)]
+        public void TestAsd(string asd)
+        {
+
         }
     }
 }
