51a2e878fe3533f421d17afa4b003ef451eaa55a
taschnue <stefan.schnuerle@hslu.ch>
2015-05-30 11:51:11 +0200

    introduced EvaluationTest project

--- EvaluationTest/Properties/AssemblyInfo.cs
+++ EvaluationTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Evaluation")]
+[assembly: AssemblyTitle("EvaluationTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Evaluation")]
+[assembly: AssemblyProduct("EvaluationTest")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("33abf631-7683-45b1-851f-828f1843069b")]
+[assembly: Guid("fdc4c185-89ec-4f8a-88a0-81bd8a35b62a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Utility")]
-[assembly: InternalsVisibleTo("EvaluationTest")]
---
+++ EvaluationTest/SetCoverageTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EvaluationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
